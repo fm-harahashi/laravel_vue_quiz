e6d749f79ee7c82391856d27e0eb663d8c00a483
harahashinaoya <mogamimaly3603@gmail.com>
2020-10-20 15:44:16 +0900

    webpack.mix.jsの移行のため、vue_hello/webpack.mix.jsをlaravel_vue_quiz/webpack.mix.jsに上書きしました。

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,14 @@
-const mix = require('laravel-mix');
+let mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.js('resources/js/home.js', 'public/js')
+    .js('resources/js/quiz.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css', {
+        implementation: require('node-sass')
+    });
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.browserSync({
+    files: [
+        "public/**/*.*"
+    ],
+    proxy: "http://localhost:8000"
+});
